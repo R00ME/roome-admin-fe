b231367cd443200d962354de132524682d4af5eb
miseullang <miseullang@gmail.com>
2025-05-02 13:57:52 +0900

    Initial commit: Vite + React + TypeScript template setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
+import Router from '@/routes/Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+function App() {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
