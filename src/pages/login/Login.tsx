c86ad4967b847b112ceb4db200eb88b73a747351
choiboa <musamea99@gmail.com>
2025-07-13 17:45:37 +0900

    🚨 Fix: fields 데이터 외부로 분리

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,37 +1,9 @@
-import emailIcon from '@/assets/icons/login/email-icon.svg';
-import passwordIcon from '@/assets/icons/login/pw-icon.svg';
 import logoBg from '@/assets/images/login-logo.svg';
 import { useState } from 'react';
 import { Card, CardContent } from '../../components/ui/card';
 import Form from './components/Form';
+import { adminLoginFields, tempPasswordFields } from './constants/fields';
 
-const adminLoginFields = [
-  {
-    id: 'email',
-    label: 'Email',
-    type: 'email',
-    placeholder: 'RoomE@example.com',
-    icon: emailIcon,
-  },
-  {
-    id: 'password',
-    label: 'Password',
-    type: 'password',
-    placeholder: 'Password',
-    icon: passwordIcon,
-  },
-];
-
-const tempPasswordFields = [
-  { id: 'name', label: 'Name', type: 'text', placeholder: '홍길동' },
-  {
-    id: 'email',
-    label: 'Email',
-    type: 'email',
-    placeholder: 'RoomE@example.com',
-    icon: emailIcon,
-  },
-];
 
 export default function Login() {
   const [formType, setFormType] = useState<'login' | 'tempPassword'>('login');
@@ -50,6 +22,7 @@ export default function Login() {
       }}
       className='relative h-screen flex items-center justify-center overflow-hidden'>
       <Card className='bg-white rounded-2xl shadow-md py-10 px-6 flex flex-col gap-5 z-100 '>
+        {/* 로그인 & 임시 비밀번호 발급 창 컴포넌트 */}
         <CardContent className='flex flex-col gap-5'>
           {isLogin ? (
             <Form
