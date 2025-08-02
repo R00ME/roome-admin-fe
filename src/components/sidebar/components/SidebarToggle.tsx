77ad60bafda97d7ea522fa01212b11479d453d55
choiboa <musamea99@gmail.com>
2025-08-02 23:53:52 +0900

    Revert "🔨 Refactor: 위치 조정 transform 으로 변경"

--- src/components/sidebar/components/SidebarToggle.tsx
+++ src/components/sidebar/components/SidebarToggle.tsx
@@ -8,17 +8,15 @@ interface SidebarToggleProps {
 const SidebarToggle = ({ isExpanded, onToggle }: SidebarToggleProps) => {
   return (
     <button
-      className={`fixed text-2xl cursor-pointer select-none bg-[#F7FBFE] opacity-50 hover:opacity-100 rounded-full top-1/2 left-0 transition-transform duration-300 ease-in-out ${
-        isExpanded
-          ? 'translate-x-43 -translate-y-81.5'
-          : 'translate-x-[88px] bg-none'
+      className={`fixed text-2xl cursor-pointer select-none bg-[#F7FBFE] opacity-50 hover:opacity-100 rounded-full ${
+        isExpanded ? 'top-6.5 left-42' : 'top-1/2 left-22 bg-none'
       }`}
       onClick={onToggle}>
       <img
         src={arrowIcon}
         alt='hamburger-menu-icon'
         className={`[-webkit-user-drag:none] select-none pointer-events-none 
-          ${isExpanded ? '' : 'scale-x-[-1]'}  
+          ${isExpanded ? '' : 'scale-x-[-1]' }  
         `}
       />
     </button>
