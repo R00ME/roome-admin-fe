3d219572af195790fb260253b2d89ad569fb65dc
choiboa <musamea99@gmail.com>
2025-08-23 17:31:01 +0900

    💄Style: 위험스코어 삭제 및 레이아웃 수정

--- src/pages/user-dashboard/components/userActivityTabs/SummaryTab.tsx
+++ src/pages/user-dashboard/components/userActivityTabs/SummaryTab.tsx
@@ -1,19 +1,12 @@
-import GenericTable from "../../../../components/GenericTable";
-import { abnormalHeaders, abnormalRows } from "../../constants/abnormalData";
 
-export default function SummaryTab({ drawerWidth }: { drawerWidth: number }) {
+export default function SummaryTab() {
   return (
     <div className="flex flex-row items-center justify-center gap-5">
       {/* 차트 */}
       <div className="flex-1 flex justify-center items-center">
         <div className="w-48 h-48 bg-blue-100 rounded-full flex items-center justify-center" />
       </div>
-      {/* 표 */}
-      {drawerWidth > 900 && (
-        <div className="flex flex-1 justify-center items-center">
-          <GenericTable headers={abnormalHeaders} rows={abnormalRows} />
-        </div>
-      )}
+      
     </div>
   );
 }
